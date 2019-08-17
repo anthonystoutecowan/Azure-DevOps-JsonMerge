3ceb2a109f823a90a708aa2da3c050d022188bd9
Ringo <ringo_kam@hotmail.com>
2019-08-17 19:50:12 -0400

    update test with mergeJson

--- src/task/merge-json.spec.ts
+++ src/task/merge-json.spec.ts
@@ -5,37 +5,58 @@ test("mergeFile with empty target file will fail", () => {
     return expect(result).rejects.toThrow("targetFile cannot be null or empty!!!")
 });
 
-test("mergeFile with empty target file will fail", () => {
+test("mergeFile with empty source file(s) will fail", () => {
     const mergeJson = require("./merge-json");
 
     const result = mergeJson.mergeFile("test.json", []);
     return expect(result).rejects.toThrow("sourceFile(s) should not be null or empty!!!")
 });
 
-test("mergeFile will merge correctly", () => {
-    //Setup   
-    jest.resetModules(); //need that if require json was done before 
-    jest.mock('../fs-helper.ts', () => {
-        const resolveFileInjson = async (filePaths: string[]): Promise<any[]> => {
-            const promise = new Promise<any[]>((resolve => {
-                resolve([{ test: "HOLA" }, { test: "Hola2", test1: "yes" }])
-            }));
-            return promise;
-        }
-        const writeJson = async (targetFilePath: string, jsonObject: Object): Promise<void> => {
-            const promise = new Promise<void>((resolve => { resolve() }));
-            return promise;
-        }
-        return {
-            writeJson,
-            resolveFileInjson
-        }
+describe("mergeFile will merge correctly", () => {
+
+    beforeEach(() => {
+        jest.resetModules(); //need that if require json was done before 
+        jest.mock('../fs-helper.ts', () => {
+            const resolveFileInjson = async (filePaths: string[]): Promise<any[]> => {
+                const [target, ...sources] = filePaths.map((file, index) => {return { fileName: file, index }})
+                const promise = new Promise<any[]>((resolve => {
+                    resolve([target, ...sources])
+                }));
+                return promise;
+            }
+            const writeJson = async (targetFilePath: string, jsonObject: Object): Promise<void> => {
+                const promise = new Promise<void>((resolve => { resolve() }));
+                return promise;
+            }
+            return {
+                writeJson,
+                resolveFileInjson
+            }
+        })
     })
-    const mergeJson = require("./merge-json");
 
-    //run
-    const resultPromsie = mergeJson.mergeFile("test.json", ["test1.json", "test2.json"]);
+    test("1 file", () => {
+        //setup
+        const mergeJson = require("./merge-json");
 
-    //Assert
-    return expect(resultPromsie).resolves.toBe(JSON.stringify({ test: "Hola2", test1: "yes" }));
+        //run
+        const resultPromsie = mergeJson.mergeFile("test.json", ["test1.json"]);
+
+        //Assert
+        const result = JSON.stringify({ fileName: "test1.json", index: 1 });
+        return expect(resultPromsie).resolves.toBe(result);
+    })
+
+    test("2 file", () => {
+        //setup
+        const mergeJson = require("./merge-json");
+
+        //run
+        const resultPromsie = mergeJson.mergeFile("test.json", ["test1.json", "test2.json"]);
+
+        //Assert
+        const result = JSON.stringify({ fileName: "test2.json", index: 2 });
+        return expect(resultPromsie).resolves.toBe(result);
+    })
+
 })
